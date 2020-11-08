f20da018a6fd49e4569da6d689bdd0910f75ef9c
Arjun Sharma <ar.sharma@mail.utoronto.ca>
2020-11-08 05:37:45 +0400

    fully working long audio with summary and gcs upload

--- interface/app.js
+++ interface/app.js
@@ -2,28 +2,35 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let multer = require('multer');
 const fs = require('fs');
-
+//var wavFileInfo = require('wav-file-info');
 //importing google speech api
 const speech = require('@google-cloud/speech');
 const client = new speech.SpeechClient();
 
-
-
-
-
 let app = express();
 app.use(bodyParser.urlencoded({extended: true}))
 
-let storage = multer.diskStorage({
+var dir = '../data';
+
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
+
+const {Storage} = require('@google-cloud/storage');
+
+// Creates a client
+const storage = new Storage();
+
+let storagemulter = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, '../data')
     },
     filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now())
+        cb(null, file.originalname);
     }
-})
+    });
 
-let upload = multer({ storage: storage })
+let upload = multer({ storage: storagemulter })
 
 
 //Home page
@@ -32,16 +39,6 @@ app.get('/',function(req,res){
 
 });
 
-// app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
-//     const file = req.file;
-//     if (!file) {
-//         const error = new Error('Please upload a file')
-//         error.httpStatusCode = 400
-//         return next(error)
-//     }
-//     res.send("your file has been uploaded")
-//
-// })
 
 app.post('/uploadfile', upload.single('myFile'), function (req, res, next) {
     console.log("success");
@@ -49,18 +46,27 @@ app.post('/uploadfile', upload.single('myFile'), function (req, res, next) {
 
 //    filename is going to be req.file.path
 async function main(){
-    const filename = req.file.path;
-
-    const fileGoogle = fs.readFileSync(filename);
+    const filepath = req.file.path;
+    const bucketName = 'newhacks_audio';
+//  const fullPath = './practice/Welcome.wav';
+//  var filename = fullPath.replace(/^.*[\\\/]/, '');
+    const gcsUri = 'gs://newhacks_audio/'+req.file.originalname;
+    const fileGoogle = fs.readFileSync(filepath);
     const audioBytes = fileGoogle.toString('base64');
-
+    // var wavinfo = await wavFileInfo.infoByFilename(filepath, function(err, info){
+    //     if (err) throw err;
+    //     console.log(info.header.sample_rate);
+    //     return info;
+    // });
+    // console.log(wavinfo);
+    //
     const audio = {
-        content: audioBytes
+        uri: gcsUri
     };
 
     const config = {
         encoding: 'LINEAR16',
-        sampleRateHertz: 16000,
+        //sampleRateHertz: wavinfo.header.sample_rate,
         languageCode: 'en-US'
     };
 
@@ -69,14 +75,24 @@ async function main(){
         config: config
     };
 
-
-    const [response] = await client.recognize(request);
+    await storage.bucket(bucketName).upload(filepath, {
+        gzip: false,
+        metadata: {
+            cacheControl: 'no-cache, max-age=0'
+        },
+    });
+    const [operation] = await client.longRunningRecognize(request);
+    const [response] = await operation.promise();
     const transcription = response.results.map(result =>
         result.alternatives[0].transcript).join('\n');
     fs.writeFileSync('../full_transcriptions/' + "input.txt", transcription, 'ascii', function (err) {
         if (err) return console.log(err);
         console.log("Transcription written to "+  "input.txt");
+
+
     });
+
+// Get a Promise representation of the final result of the job
 
 
     console.log("Running python");
@@ -90,6 +106,8 @@ async function main(){
     });
 
 }
+
+
 
     //calling the async function above
     main().catch(console.error);
